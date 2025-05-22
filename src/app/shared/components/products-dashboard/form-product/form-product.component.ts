98b788179a2e08f0e73df7c232f765adadfd4640
akash kale <kale64136@gmail.com>
2025-05-22 21:34:58 +0530

    fix the bug

--- src/app/shared/components/products-dashboard/form-product/form-product.component.ts
+++ src/app/shared/components/products-dashboard/form-product/form-product.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-product',
@@ -8,26 +7,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class FormProductComponent implements OnInit {
 
-  myForm: FormGroup;
+  constructor() { }
 
-  constructor(private fb: FormBuilder) {
-    this.myForm = this.fb.group({
-      name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      age: [null, [Validators.required, Validators.min(1)]]
-    });
-  }
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
   }
-
-  onSubmit() {
-    if (this.myForm.valid) {
-      console.log('Form Submitted:', this.myForm.value);
-    } else {
-      console.log('Form Invalid');
-    }
-  }
-
 
 }
