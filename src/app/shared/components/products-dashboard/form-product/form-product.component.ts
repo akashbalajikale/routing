59441b5fabadc54fc21241c79123f7299738e5d8
akash kale <kale64136@gmail.com>
2025-05-26 23:22:32 +0530

    work on

--- src/app/shared/components/products-dashboard/form-product/form-product.component.ts
+++ src/app/shared/components/products-dashboard/form-product/form-product.component.ts
@@ -6,13 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-product.component.scss']
 })
 export class FormProductComponent implements OnInit {
- cricketers = [
-    { name: 'Virat Kohli', country: 'India', runs: 12000 },
-    { name: 'Joe Root', country: 'England', runs: 10000 },
-    { name: 'Babar Azam', country: 'Pakistan', runs: 8000 },
-    { name: 'Kane Williamson', country: 'New Zealand', runs: 9500 },
-    { name: 'Steve Smith', country: 'Australia', runs: 9200 }
-  ];
+ 
   constructor() { }
 
   ngOnInit(): void {
