d7edad91cd06c53e0312a6a3eefd613e414bc9b7
akash kale <kale64136@gmail.com>
2025-05-26 23:21:35 +0530

    add cricekter data

--- src/app/shared/components/user/user.component.ts
+++ src/app/shared/components/user/user.component.ts
@@ -7,36 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  skillsbox : Array<any>= [
-  {
-    skillname :'nodejs',
-    skillInfo :`<strong>nodejs</strong> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi sint ea totam ducimus voluptas? Quam inventore necessitatibus doloremque id modi consectetur. Reprehenderit, eos incidunt facilis natus perferendis quia quaerat voluptas!
-`
-  },
-  {
-    skillname :'express',
-    skillInfo :`<strong>express</strong> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi sint ea totam ducimus voluptas? Quam inventore necessitatibus doloremque id modi consectetur. Reprehenderit, eos incidunt facilis natus perferendis quia quaerat voluptas!
-`
-  },
-  {
-    skillname :'mysql',
-    skillInfo :`<strong>mysql</strong> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi sint ea totam ducimus voluptas? Quam inventore necessitatibus doloremque id modi consectetur. Reprehenderit, eos incidunt facilis natus perferendis quia quaerat voluptas!
-`
-  },
-  {
-    skillname :'springboot',
-    skillInfo :`<strong>springboot</strong> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi sint ea totam ducimus voluptas? Quam inventore necessitatibus doloremque id modi consectetur. Reprehenderit, eos incidunt facilis natus perferendis quia quaerat voluptas!
-`
-  }
- ]
-
- defaultskill : string ='express'
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  onClick(skill :any){
-    this.defaultskill=skill.skillname
-  }
 }
