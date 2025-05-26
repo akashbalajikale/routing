6d6f9c9b6bca190aa901f150565947c099b0b381
akash kale <kale64136@gmail.com>
2025-05-26 23:43:25 +0530

    initial commit

--- src/app/shared/components/products-dashboard/form-product/form-product.component.ts
+++ src/app/shared/components/products-dashboard/form-product/form-product.component.ts
@@ -7,15 +7,9 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./form-product.component.scss']
 })
 export class FormProductComponent implements OnInit {
- 
- cricketers = [
-    { name: 'Virat Kohli', country: 'India', runs: 12000 },
-    { name: 'Joe Root', country: 'England', runs: 10000 },
-    { name: 'Babar Azam', country: 'Pakistan', runs: 8000 },
-    { name: 'Kane Williamson', country: 'New Zealand', runs: 9500 },
-    { name: 'Steve Smith', country: 'Australia', runs: 9200 }
-  ];
-  stdform: any;
+OnStdAdd() {
+throw new Error('Method not implemented.');
+}  
 
   constructor(
     
@@ -24,9 +18,6 @@ export class FormProductComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  OnStdAdd(){
-     
-  }
-
+ 
 
 }
